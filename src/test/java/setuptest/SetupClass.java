21f8c57f1dbb28d00199e49b0eface1285f736e4
srikanthr2 <srikanth.chincholi@gmail.com>
2017-10-29 10:52:07 +0530

    added a line

--- src/test/java/setuptest/SetupClass.java
+++ src/test/java/setuptest/SetupClass.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-
+// line checks into master branch
 public class SetupClass {
 	
 	public static WebDriver d;
